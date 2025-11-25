cd439394fb65ffe242f823c1cdeae23ef903052d
piki <piki@90e39bfa-ee77-4dea-bf83-b1b8ca614190.attlocal.net>
2025-11-25 14:29:26 -0600

    auto-update 2025-11-25 14:29:26

--- AdvanceIrrigationStrategy.java
+++ AdvanceIrrigationStrategy.java
@@ -6,7 +6,8 @@ public class AdvanceIrrigationStrategy extends IrrigationStrategy {
     
 }
 
-    public void adjustIrrigationSchedule(){
+    public void adjustIrrigationSchedule()
+    {
 
     }
 
